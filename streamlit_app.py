d1bde0d58bf85df10d37410e582e66f2e4ce6e27
artinmajdi <msm2024@gmail.com>
2022-09-13 18:28:51 -0700

    deploying to streamlit server

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,10 @@
+from faulthandler import disable
 import streamlit as st
 import pandas as pd
 import numpy as np
 from funcs import Connect_To_PACS
-import pydicom
 from pynetdicom import AE, sop_class
-from pydicom import uid
+# from pydicom import uid
 
 
 class App(Connect_To_PACS):
@@ -16,14 +16,12 @@ class App(Connect_To_PACS):
 
         Connect_To_PACS.__init__(self, port=104, ae_title='AET')
 
-
     def getQueryRetrieveLevel(self):
 
         st.sidebar.markdown('## Parameters')
         cols = st.sidebar.columns(2)
-        self.queryRetrieveLevel = cols[0].radio(label='Select the Query Retrieval Level', options=['STUDY' ,'PATIENT'], horizontal=True , )
+        self.queryRetrieveLevel   = cols[0].radio(label='Select the Query Retrieval Level', options=['STUDY' ,'PATIENT'], horizontal=True , )
         self.queryRetrieveLevel2 = cols[1].radio(label='', options=['SERIES', 'IMAGE'], horizontal=True , disabled=True)
-
 
     # QueryRetrieveLevel
     def getRequestedContext(self):
@@ -36,38 +34,56 @@ class App(Connect_To_PACS):
         elif self.queryRetrieveLevel == 'PATIENT':
             output = st.sidebar.radio('PatientRoot or PatientStudy', ['Root', 'StudyOnly'], horizontal=True)
             reqContext = 'Patient' + output + 'QueryRetrieveInformationModel'
-        else:
-            st.write('These options are not currently available')
 
+        cols = st.sidebar.columns(2)
+        self.reqContextAction   = cols[0].radio('Select the Requested Context', ['Find', 'Get',], horizontal=True)
+        self.reqContextAction2 = cols[1].radio('', ['Move'], horizontal=True, disabled=True)
+        reqContext = reqContext + self.reqContextAction
+        self.requestedContext = eval( f'sop_class.{reqContext}' )
 
-        st.sidebar.markdown('## Action')
-        if self.queryRetrieveLevel in ['STUDY', 'PATIENT']:
+        st.markdown('## SOP Class UID')
+        st.success(f'SOP Class: \n - {reqContext}  \n - {self.requestedContext}')
 
-            self.reqContextAction = st.sidebar.radio('Select the Requested Context', ['Find', 'Get', 'Move'], horizontal=True)
-            reqContext = reqContext + self.reqContextAction
+        if self.reqContextAction =='Find':
+            self.do_c_find()
 
-            self.requestedContext = eval( f'sop_class.{reqContext}' )
-
-            st.sidebar.success(f'SOP Class: \n - {reqContext}  \n - {self.requestedContext}')
 
 
     def do_c_find(self):
 
+        st.markdown('## Founded subjects')
         self.send_c_find(show_results=False, queryRetrieveLevel=sidebar.queryRetrieveLevel, requestedContext=sidebar.requestedContext)
         st.markdown( f'Number of retrieved subjects: {len(self.list_sample_info)}' )
 
         cols = st.columns(2)
         index = cols[0].slider("Select the subject index to display it's status", min_value=0, max_value=len(self.list_sample_info)-1, value=0, step=1, format=None, key=None, help=None, on_change=None, args=None, kwargs=None)
-
         cols[1].markdown(self.list_sample_info[index][0])
 
+    def get_download_info(self):
 
+        self.searchType = st.sidebar.radio('Search type', ['AccessionNumber', 'PatientID'], horizontal=True)
+
+        file = st.sidebar.file_uploader('CSV file containing subjects identifier information ',  type='csv')
+        if file is not None:
+            self.df = pd.read_csv(file)
+            st.expander('CSV file containing subjects identifier information').write(self.df)
+
+        self.output_dir = st.sidebar.text_input('Output Directory' ,   value='/Users/personal-macbook/Documents/projects/D7.PACS/code/Data7.PACS_DICOM')
+
+
+        self.timelag = st.sidebar.number_input('Enter the timelag between each download in seconds', min_value=0, max_value=100, value=60, step=1, format=None, key=None, help=None, on_change=None, args=None, kwargs=None)
+
+        cols = st.sidebar.columns(2)
+        self.startButton =  cols[0].button('Start Download')
+
+        if self.startButton:
+            i,n = 1,100
+            cols[1].write( f'Download Progress: {i}/{n}' )
 
 sidebar = App()
 sidebar.getQueryRetrieveLevel()
 sidebar.getRequestedContext()
-sidebar.do_c_find()
-
+sidebar.get_download_info()
 
 
 
